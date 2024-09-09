cff42b49b99157007392fa54db29004872fbd0c2
Breiddyvillanueva <bvillanueva55@itfip.edu.co>
2024-09-09 12:43:54 -0500

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,38 @@
+<ul class="nav flex-column">
+            <li class="nav-item">
+              <a href="prematricula.php">
+                <svg class="bi"><use xlink:href="#house-fill"/></svg>
+                Prematricula
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="financiera.php">
+                <svg class="bi"><use xlink:href="#file-earmark"/></svg>
+                Matricula Financiera
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="horarios.php">
+                <svg class="bi"><use xlink:href="#cart"/></svg>
+               Horarios
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="notas.php">
+                <svg class="bi"><use xlink:href="#people"/></svg>
+              Notas
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="#">
+                <svg class="bi"><use xlink:href="#graph-up"/></svg>
+                Reports
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="#">
+                <svg class="bi"><use xlink:href="#puzzle"/></svg>
+                Integrations
+              </a>
+            </li>
+          </ul>
